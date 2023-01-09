aee00bde318c47864ba17e71d30f785b2f0fcc4f
MinseoKangQ <kms02171@naver.com>
2023-01-10 00:02:59 +0900

    컨트롤러 구현

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,9 +1,14 @@
 package org.example;
 
 import org.example.repository.TodoRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+@SpringBootApplication
 public class TodoServerApplication {
     public static void main(String[] args) {
-        System.out.println("HELLO TODO");
+        SpringApplication.run(TodoServerApplication.class, args);
     }
 }
